8c40588778ef7ca6897cd07a6f76b984e4de357a
Mihir Kulshreshtha <75397268+Miwi343@users.noreply.github.com>
2026-09-01 14:54:08 -0400

    Solve gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -6,10 +6,12 @@ def gradient_descent_quadratic(a: float, b: float, c: float, x0: float, lr: floa
     def gradient(x):
         """
         finds gradient at x
+        know gradient of ax^2 + bx + c -> 2ax + b
         """
         return 2*a*x + b
 
     # step in negative direction of gradient
+    # find gradient and then step
     for _ in range(steps):
         grad = gradient(x0)
         x0 = x0 - grad * lr
