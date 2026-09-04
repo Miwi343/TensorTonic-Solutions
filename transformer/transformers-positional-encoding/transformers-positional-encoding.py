c13c2fee93ac29c163b453369c6ea95c70cf9b0e
Mihir Kulshreshtha <75397268+Miwi343@users.noreply.github.com>
2026-09-04 14:33:57 -0400

    Solve transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -7,12 +7,16 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
     pe = np.zeros((seq_length, d_model), dtype=float)
     
     positions = np.arange(seq_length, dtype=float)[:,None]
+    # shape is (seq_len, 1)
 
     even = np.arange(start=0, stop=d_model, step=2, dtype=float)
-
+    # shape is (d_model / 2)
     inv_freq = 1.0 / (10000 ** (even/d_model))
+    # shape is (d_model / 2)
     
     pe[:, 0::2] = np.sin(positions * inv_freq)
+    # broadcasting inv_freq (d_model / 2) -> (1, d_model / 2)
+    # shape is (seq_len, d_model / 2)
     pe[:, 1::2] = np.cos(positions * inv_freq)
     
     return pe
